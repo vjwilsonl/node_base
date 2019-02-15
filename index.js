91fe3c841fe6ec48a2ac5a41108ef0b78b26df51
vjwilsonl <vjwilsonl@gmail.com>
2019-02-15 18:04:03 +0800

    FIRST CODE INIT - W google auth and cookie

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
+const passport = require('passport');
+const keys = require('./config/keys');
+require('./models/User.js');
+require('./services/passport');
+
+mongoose.connect(keys.mongoURI);
+
+const app = express();
+
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
+
+//client id
+//secret
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(5000);
---
+++ services/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  //for cookie
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  //for cookie
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: keys.googleClientID,
+      clientSecret: keys.googleClientSecret,
+      callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOne({ googleId: profile.id }).then(existingUser => {
+        if (existingUser) {
+          // already have record
+          done(null, existingUser);
+        } else {
+          new User({
+            googleId: profile.id
+          })
+            .save()
+            .then(user => done(null, user));
+        }
+      });
+    }
+  )
+);

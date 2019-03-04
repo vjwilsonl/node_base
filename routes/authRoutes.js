414fed2bedd67363a27592cdf7130de85d7a57ca
vjwilsonl <vjwilsonl@gmail.com>
2019-03-04 21:28:28 +0800

    Merge all path to API/

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,13 +2,13 @@ const passport = require('passport');
 
 module.exports = app => {
   app.get(
-    '/auth/google',
+    '/api/auth/google',
     passport.authenticate('google', {
       scope: ['profile', 'email']
     })
   );
 
-  app.get('/auth/google/callback', passport.authenticate('google'));
+  app.get('/api/auth/google/callback', passport.authenticate('google'));
 
   app.get('/api/current_user', (req, res) => {
     res.send(req.user);

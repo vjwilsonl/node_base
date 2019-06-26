895fe33a635d07bb420bd02163589b6dc19c534c
vjwilsonl <vjwilsonl@gmail.com>
2019-06-26 17:21:04 +0800

    edit create story

--- routes/storyRoutes.js
+++ routes/storyRoutes.js
@@ -1,3 +1,9 @@
+/**
+ *
+ * @storyRoutes.js
+ * Provides API end points for Story
+ *
+ */
 const passport = require('passport');
 const mongoose = require('mongoose');
 const fs = require('fs');
@@ -16,7 +22,9 @@ AWS.config.update({
   secretAccessKey: keys.awsSecretKey
 });
 
-//file upload lib
+/**
+ * file upload library
+ */
 const multer = require('multer');
 const storage = multer.diskStorage({
   destination: function(req, file, cb) {
@@ -28,43 +36,66 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage: storage });
 
+function uploadFileParams(filePath) {
+  const buffer = readChunk.sync(filePath, 0, fileType.minimumBytes);
+  var fileMeta = fileType(buffer);
+  /**
+   * configuring file parameters
+   */
+  var params = {
+    Bucket: 'tusi360',
+    Body: fs.createReadStream(filePath),
+    Key: 'storyimage/' + Date.now() + '_' + path.basename(filePath),
+    ACL: 'public-read',
+    ContentType: fileMeta.mime
+  };
+  //begin uploading
+  return params;
+  // return s3.upload(params, (err, data) => {
+  //   console.log('hehehehe');
+  //   resolve('test');
+  // });
+  // , (err, data)
+  //  => {
+  //   if (err) {
+  //     res.send('Error', err);
+  //   }
+  //   if (data) {
+  //     fs.unlink(filePath);
+  //     console.log(data, 'this is function data');
+  //     return data;
+  //   }
+  // });
+}
+
 module.exports = app => {
-  app.post('/api/story', upload.single('vrfile'), (req, res) => {
+  /**
+   * POST /api/story
+   * Create new story
+   */
+  app.post('/api/story', upload.single('vrfile'), async (req, res) => {
     const { title, description } = req.body;
-    var s3 = new AWS.S3();
 
     if (req.file) {
-      var filePath = process.cwd() + '/' + req.file.path;
-      const buffer = readChunk.sync(filePath, 0, fileType.minimumBytes);
-
-      var fileMeta = fileType(buffer);
-      //configuring file paremeters
-      var params = {
-        Bucket: 'tusi360',
-        Body: fs.createReadStream(filePath),
-        Key: 'storyimage/' + Date.now() + '_' + path.basename(filePath),
-        ACL: 'public-read',
-        ContentType: fileMeta.mime
-      };
-      s3.upload(params, async (err, data) => {
+      const filePath = process.cwd() + '/' + req.file.path;
+      const params = uploadFileParams(filePath);
+      var fileData = '';
+      var s3 = new AWS.S3();
+      //begin upload
+      await s3.upload(params, async (err, data) => {
         if (err) {
+          // error in upload
           res.send('Error', err);
-        }
-
-        if (data) {
-          var vrx = new Story({
+        } else if (data) {
+          fs.unlink(filePath);
+          var story = await new Story({
             title,
             description,
             imageUrl: data.Location,
-            _user: 'req.user.id'
-          });
-          try {
-            await vrx.save();
-            res.send(vrx);
-          } catch (err) {
-            res.status(422).send(err);
-          }
-          fs.unlink(filePath);
+            _user: '5c7f539b7c34db3024c6795b'
+          }).save();
+          //respond with newest copy of story created
+          res.send(story);
         }
       });
     } else {
@@ -72,8 +103,49 @@ module.exports = app => {
       res.send('Please upload an image');
     }
   });
+
+  /**
+   * GET /api/story
+   * Retrieve story list
+   */
   app.get('/api/story', async (req, res) => {
-    const story = await Story.find({ _user: req.user.id });
+    const story = await Story.find({ _user: req.user.id }).select('-_user');
     res.send(story);
+  });
+
+  /**
+   * GET /api/story/:id
+   * Retrieve story details
+   */
+  app.get('/api/story/:id', async (req, res) => {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      try {
+        var story = await Story.findById(req.params.id);
+      } catch (err) {
+        res.status(422).send(err);
+      }
+      res.send(story);
+    } else {
+      res.send('Invalid story ID');
+    }
+    // Need to control user based story
+  });
+  /**
+   * POST /api/story/:id
+   * UPDATE story details
+   */
+  app.post('/api/story/:id', async (req, res) => {
+    if (mongoose.Types.ObjectId.isValid(req.params.id)) {
+      try {
+        var story = await Story.findById(req.params.id);
+      } catch (err) {
+        res.status(422).send(err);
+      }
+      res.send(story);
+    } else {
+      res.send('Invalid story ID');
+    }
+
+    // Need to control user based story
   });
 };
